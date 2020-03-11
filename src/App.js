7baf07b477d313c2fbe837917baf982b194380de
Argosa <ronald.kratochwill@live.com>
2020-03-11 09:58:04 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
+const ImportButton = (props) => {
+
+  return (
+    <button onClick={props.getPokemons}>Get Pokemons</button>
+  )
+}
+
+const DisplayPokemons = (props) => {
+  return (
+    <div>
+      <ul>
+        Names
+        {props.pokemon.map((poke, i) => 
+          <li key={i}>
+            {poke.name}
+          </li>
+        )}
+      </ul>
+    </div>
+  )
+}
+
 function App() {
+  const [ pokemons, setPokemons] = useState([])
+  const apiUrl = 'https://pokeapi.co/api/v2/pokemon?limit=807&offset=30';
+  const onClickHandler = () => {
+    axios.get(apiUrl)
+    .then((response) => {setPokemons(response.data.results)})
+    .catch((error) => {console.log(error)})
+    .then(() => {})
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ImportButton getPokemons={onClickHandler}/>
+      <DisplayPokemons pokemon={pokemons} />
     </div>
   );
 }
